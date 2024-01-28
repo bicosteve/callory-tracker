4fd8c5f264cd8845f248ac15073faadfec4974ab
BOloo <bicosteve4@gmail.com>
2024-01-28 18:47:06 +0300

    Added context

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/bicosteve/callory-tracker/pkg/models"
 	"github.com/justinas/nosurf"
 	"net/http"
 )
@@ -45,7 +47,7 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 func (app *application) requireAuthenticatedUser(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// if user not authenticated, redirect to login page
-		if app.isAuthenticatedUser(r) == 0 {
+		if app.isAuthenticatedUser(r) == nil {
 			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 			return
 		}
@@ -65,4 +67,32 @@ func noSurf(next http.Handler) http.Handler {
 	})
 
 	return csrfHandler
+}
+
+// authenticate retrieves users id from the session data
+// checks the db to confirm the userid corresponds to user using UserModel.Exists()
+// updates request context to include an isAuthenticatedContextKey key with true value
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// retrieve authenticated user id value from session
+		// returns 0 if no authenticatedUserId
+		exists := app.session.Exists(r, "userId")
+		if !exists {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		user, err := app.users.GetUserDetails(app.session.GetInt(r, "userId"))
+		if err == models.ErrNoRecord {
+			app.session.Remove(r, "userId")
+			next.ServeHTTP(w, r)
+			return
+		} else if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), contextKeyUser, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
